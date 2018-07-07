3b6ceaaefa62c46df59de730c40291108c2132c3
Hongrui Zheng <billyzheng.bz@gmail.com>
2018-07-07 03:26:15 -0400

    successfully send image over rest and returns embeddings

--- config/config.py
+++ config/config.py
@@ -10,16 +10,16 @@ SERVER_PORT = '5000'
 # TF SERVING SETTINGS
 TF_HOST = 'localhost'
 TF_PORT = '8500'
-MODEL_SPEC_NAME = '...'
+MODEL_SPEC_NAME = 'saved_model'
 
 # PREPROCESSOR SETTINGS
 USE_DLIB = True
 USE_MTCNN = False
-IMAGE_SIZE = 200
+IMAGE_SIZE = 160
 MIN_SIZE = 20
 MARGIN = 44
 GPU_FRACTION = 1.0
 MTCNN_THRESHOLD = [0.6, 0.7, 0.7]
 MTCNN_FACTOR = 0.709
-DETECTOR_PATH = '../model/mmod_human_face_detector.dat'
-PREDICTOR_PATH = '../model/shape_predictor_5_face_landmarks.dat'
+DETECTOR_PATH = '/home/billy/sanus_face_server/model/mmod_human_face_detector.dat'
+PREDICTOR_PATH = '/home/billy/sanus_face_server/model/shape_predictor_5_face_landmarks.dat'
--- scripts/inference_test.py
+++ scripts/inference_test.py
@@ -14,7 +14,7 @@ stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
 cnn_detector = dlib.cnn_face_detection_model_v1('/home/billy/sanus_face_server/model/mmod_human_face_detector.dat')
 sp = dlib.shape_predictor('/home/billy/sanus_face_server/model/shape_predictor_5_face_landmarks.dat')
-image = dlib.load_rgb_image('/home/billy/Downloads/wjx_test_profile_blurry.jpg')
+image = dlib.load_rgb_image('/home/billy/Downloads/wjx_test_far_extreme.jpg')
 dets = cnn_detector(image, 1)
 print(len(dets))
 faces = dlib.full_object_detections()
@@ -25,7 +25,7 @@ b, h, w, c = faces_list.shape
 print(b, h, w, c)
 request = predict_pb2.PredictRequest()
 request.model_spec.name = 'saved_model'
-request.inputs['in'].CopyFrom(tf.make_tensor_proto(faces_list, dtype=tf.float32, shape=[b, h, w, c]))
+request.inputs['in'].CopyFrom(tf.make_tensor_proto(faces_list, dtype=tf.float32))
 request.inputs['phase'].CopyFrom(tf.make_tensor_proto(False, dtype=tf.bool))
 result = stub.Predict(request, 10.0)
 print(result)
