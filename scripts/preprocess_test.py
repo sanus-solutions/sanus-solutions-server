bc02aaaf937aa47c7d133cc232adc33aefc5b9e8
Hongrui Zheng <billyzheng@Hongruis-MBP.fios-router.home>
2018-07-09 16:03:25 -0400

    added pip requirements

--- scripts/preprocess_test.py
+++ scripts/preprocess_test.py
@@ -4,10 +4,10 @@ import numpy as np
 dlib preprocessing testing script
 """
 
-cnn_detector = dlib.cnn_face_detection_model_v1('/home/billy/sanus_face_server/model/mmod_human_face_detector.dat')
-sp = dlib.shape_predictor('/home/billy/sanus_face_server/model/shape_predictor_5_face_landmarks.dat')
+cnn_detector = dlib.cnn_face_detection_model_v1('~/sanus_face_server/model/mmod_human_face_detector.dat')
+sp = dlib.shape_predictor('~/sanus_face_server/model/shape_predictor_5_face_landmarks.dat')
 window = dlib.image_window()
-image = dlib.load_rgb_image('/home/billy/Downloads/wjx_test_profile_blurry.jpg')
+image = dlib.load_rgb_image('~/face_tests/wjx_test_profile_blurry.jpg')
 dets = cnn_detector(image, 1)
 print(len(dets))
 faces = dlib.full_object_detections()
