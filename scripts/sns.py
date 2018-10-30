aa34de8f243532d396810b1ea4980c812c2f37e3
klaus <klauszeng@gmail.com>
2018-10-30 14:21:03 -0400

    added sns

---
+++ scripts/sns.py
@@ -0,0 +1,18 @@
+import boto3
+
+# Create an SNS client
+sns = boto3.client(
+	'sns',
+	aws_access_key_id='AKIAIL7SBOJ2DA3ONVKQ',
+    aws_secret_access_key='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',
+    region_name='us-east-1'
+    )
+
+# Send a SMS message to the specified phone number
+response = sns.publish(
+    # PhoneNumber='1-678-524-6213',
+    Message='Sanus Solutions Alert: Luka, you need to focus now.',
+)
+
+# Print out the response
+print(response)
