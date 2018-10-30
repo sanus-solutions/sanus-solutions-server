e322ee46532cad6dcdcc38cbe9eb15550b2f8881
klaus <klauszeng@gmail.com>
2018-10-30 14:21:15 -0400

    added sns

--- scripts/rest_inference_test.py
+++ scripts/rest_inference_test.py
@@ -3,15 +3,13 @@ import numpy as np
 import dlib
 import base64
 import ast
-import scipy.misc
 import time
 
 url = 'http://localhost:5000/sanushost/api/v1.0/entry_img'
 # url = 'http://localhost:5000/sanushost/api/v1.0/sanitizer_img'
 # image = dlib.load_rgb_image('/home/billyzheng/Downloads/klaus2.jpg')
 
-image = dlib.load_rgb_image('fuccboi2.png')
-image = scipy.misc.imresize(image, 0.8)
+image = dlib.load_rgb_image('rupert.png')
 
 shape_string = str(image.shape)
 image = image.astype(np.float64)
