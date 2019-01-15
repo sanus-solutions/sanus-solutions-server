e2277672d648821a795dd17b6edd6c16332c10e6
billyzheng <billyzheng.bz@gmail.com>
2019-01-15 01:47:58 -0500

    added check staff

--- custom_clients/graph.py
+++ custom_clients/graph.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
+import numpy as np
 from sanus_face_server.config import config
-from sanus_face_server.custom_clients import id_client
+
 
 class Graph():
     """
@@ -8,48 +9,66 @@ class Graph():
         id, list of neighbors, list of latest ts, list of latest emb
     """
 
-    def __init__(self, id_client):
+    def __init__(self):
         self.client = MongoClient(config.MONGO_HOST, config.MONGO_PORT)
         self.db = self.client[config.MONGO_DB_NAME]
         self.collection = self.db[config.MONGO_DB_COL]
         self.staff_collection = self.db['staff']
 
+        # constants
+        self.EUC_THRESH = 1.0
+
     def add_node(self, name, n):
         # TODO: automatically finds the neighbors?
         # add a node in the graph with id and neighbors, ts and emb empty
         # name: int id
-        # neighbors: list of int ids
-        # returns 0 if success
-        
+        # neighbors: list of int ids        
         # check if node exists
         current_count = self.collection.count({id: name})
         # count should be either 1 or 0
+        if current_count != 0 || current_count != 1:
+            return 'failed'
         if !current_count:
             # no previous record
-            new_node = {id: name, neighbors: n, timestamps: [], embeddings:[]}
-            self.collection.insert(new_node)
-            return 0
+            try:
+                new_node = {id: name, neighbors: n, timestamps: [], embeddings:[]}
+                self.collection.insert(new_node)
+                return 'success'
+            except:
+                return 'failed'
         else:
             # previous record exists, merge the neighbors
-            rec = self.collection.find({id: name})
-            new_neighbors = set(rec['neighbors'] + n)
-            self.collection.update_one({id: name}, {'$set': {neighbors: new_neighbors}})
-            return 0
+            try:
+                rec = self.collection.find({id: name})
+                new_neighbors = set(rec['neighbors'] + n)
+                self.collection.update_one({id: name}, {'$set': {neighbors: new_neighbors}})
+                return 'success'
+            except:
+                return 'failed'
 
-        return 'lolshit'
 
     def remove_node(self, name):
-        self.collection.delete_one({id: name})
-        return 0
+        try:
+            self.collection.delete_one({id: name})
+            return 'success'
+        except:
+            return 'failed'
 
     def add_staff(self, emb, name):
         # staff should be stored in a separate collection
-        new_staff = {id: name, embedding: emb}
         try:
+            new_staff = {id: name, embedding: emb}
             self.staff_collection.insert(new_staff)
             return 'success'
         except:
             return 'failed'
+
+    def check_staff(self, emb):
+        for rec in self.staff_collection.find():
+            dist = self.euclidean_distance(rec['embeddings'], emb)
+            if dist < self.EUC_THRESH:
+                return (True, rec['id'])
+        return (False, None)
 
     def remove_staff(self, name):
         try:
@@ -59,18 +78,31 @@ class Graph():
             return 'failed'
 
     def update_node(self, name, embedding, timestamp):
-        rec = self.collection.find({id: name})
-        new_timestamps = rec['timestamps'].append(timestamp)
-        new_embeddings = rec['embeddings'].append(embedding)
-        self.collection.update_one({id: name}, {"$set": {timestamps: new_timestamps, embeddings: new_embeddings}})
-        return 0
+        try:
+            rec = self.collection.find({id: name})
+            new_timestamps = rec['timestamps'].append(timestamp)
+            new_embeddings = rec['embeddings'].append(embedding)
+            self.collection.update_one({id: name}, {"$set": {timestamps: new_timestamps, embeddings: new_embeddings}})
+            return 'success'
+        except:
+            return 'failed'
 
     def check_breach(self, embeddings, timestamp, id):
         # TODO: how many depths to check? Only immediate neighbors?
+        # first check staff, if not staff, no breach
         # there should only be one record under the id
         rec = self.collection.find({name: id})
         for neighbor in rec['neighbors']:
             for emb in self.collection.find({name: neighbor})['embeddings']:
-
+                staff, id = self.check_staff(emb)
+                if !staff:
+                    return False
+                else:
+                    return 'something'
         return 0
 
+
+
+    # helpers
+    def distance(self, emb1, emb2):
+        return np.linalg.norm(emb1 - emb2)
