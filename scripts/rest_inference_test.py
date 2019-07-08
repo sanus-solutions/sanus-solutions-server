3d5b0d481f9c84bdf56362af3d6c3a4e04d09b2e
rupertardo <klauszeng@gmail.com>
2019-07-08 11:08:35 -0400

    Fix bugs. Fully working

--- scripts/rest_inference_test.py
+++ scripts/rest_inference_test.py
@@ -6,8 +6,7 @@ import ast
 import time
 import json 
 
-url = 'http://192.168.0.101:5000/sanushost/api/v1.0/entry_img2'
-# url = 'http://localhost:5000/sanushost/api/v1.0/sanitizer_img'
+url = 'http://localhost:5000/sanushost/api/v1.0/entry_img'
 # image = dlib.load_rgb_image('/home/billyzheng/Downloads/klaus2.jpg')
 
 image = np.asarray(Image.open('luka.png'), dtype=np.uint8)
@@ -19,7 +18,7 @@ image = image.astype(np.float64)
 # image_temp = np.reshape(image, (1, image.shape[0]*image.shape[1]*image.shape[2]))
 img_64 = base64.b64encode(image).decode('ascii')
 # payload = {"NodeID": "demo_sanitizer", "Timestamp": [time.time()], "Image": img_64, "Shape": shape_string}
-payload = {"NodeID": "demo_entry", "Timestamp": time.time(), "Image": [img_64], "Shape": shape_string}
+payload = {"NodeID": "demo_entry", "Timestamp": time.time(), "Image": img_64, "Shape": shape_string}
 
 headers = {'Content-Type': 'application/json', 'Accept': 'text/plain'}
 result = requests.post(url, json=payload, headers=headers)
