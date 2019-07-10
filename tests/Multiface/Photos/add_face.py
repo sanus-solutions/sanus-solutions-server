1d4cc976128e76d8fac48034d9dc2af595a5e97b
Klaus <klauszeng@gmail.com>
2019-07-10 15:17:25 -0400

    new add face client for testing

---
+++ tests/Multiface/Photos/add_face.py
@@ -0,0 +1,13 @@
+import os, base64, requests
+import numpy as np
+from PIL import Image
+
+url = 'http://172.29.135.101:5000/sanushost/api/v1.0/add_face'
+headers = {"Content-Type": "application/json", "Accept": "text/plain"}
+
+image = np.asarray(Image.open('rupert.png'), dtype=np.uint8)
+shape_string = str(image.shape)
+image = image.astype(np.float64)
+img_64 = base64.b64encode(image).decode('ascii')
+payload = {"Image": img_64, "Shape": shape_string, "ID": 'rupert'}
+result = requests.post(url, json=payload, headers=headers)
