3cfbb5b449d0b528c706bc7cec1ac93c740d981a
rupertardo <klauszeng@gmail.com>
2019-07-11 15:15:59 -0400

    mongo client

--- mongo/mongo_client.py
+++ mongo/mongo_client.py
@@ -2,15 +2,41 @@ import pymongo
 
 class MongoClient():
 
-    def __init__(self, port='mongodb://localhost:27017/'):
+    def __init__(self, db_name, port='mongodb://172.29.135.101:27017/'):
         self.client = pymongo.MongoClient(port)
-        return
+        self.db = self.client[db_name]
+        print(db_name, "Database created")
+        self.face_collection = self.db['FaceCollection']
+        print("FaceCollection initialized")
 
+    def add_staff(self, adict):
+        result = self.face_collection.insert_one(adict)
+        return result.inserted_id
 
+    def get_embedding_by_name(self, name):
+        query = {'Name' : name}
+        result = self.face_collection.find_one(query)['Embedding']
+        return result
 
-    def check_connection(self, ):
-        return
+    def remove_staff(self, name):
+        query = {'Name' : name}
+        result = self.face_collection.delete_one(query)
+        return result
 
-if __name__ == '__main__':
-    client = MongoClient()
-    print("success")
+    def find_all(self,):
+        return self.face_collection.find()
+
+    def delete_all(self):
+        self.face_collection.delete_many({})
+
+# if __name__ == '__main__':
+#     client = MongoClient('hospital')
+    # result = client.add_staff({'Name': "luka", "Embedding": "wadafuq"})
+    # result = client.add_staff({'Name': "luka3", "Embedding": "wadafuq"})
+    # result = client.add_staff({'Name': "luka4", "Embedding": "wadafuq"})
+    # result = client.get_embedding_by_name('luka')
+    # result = client.remove_staff('luka')
+    # client.delete_all()
+    # result = client.find_all()
+    # for x in result:
+    #     print(x)
