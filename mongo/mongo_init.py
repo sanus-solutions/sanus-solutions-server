5c31a8a7db395e62d6f5fd0b1b06e62251062e56
Klaus <klauszeng@gmail.com>
2019-07-21 21:31:54 -0400

    finish basic setup/test

--- mongo/mongo_init.py
+++ mongo/mongo_init.py
@@ -17,19 +17,22 @@ class TestMongoDB(unittest.TestCase):
         self.client = pymongo.MongoClient(self.port)
         self.db = self.client[self.db_name]
         self.emb_collection = self.db[self.emb_collection_name]
-        # hygiene_collection = db[hygiene_collection_name]
-        # emb_collection.create_index('Staff', unique=True)
-        # hygiene_collection.create_index('Timestamp', expireAfterSeconds=30)
+        self.hygiene_collection = self.db[self.hygiene_collection_name]
 
     def test_connection(self,):
         self.assertEqual(self.db.command("serverStatus")["connections"]['active'], 1)
-        
-    def test__parse_url(self):
-        self.assertEqual(('localhost', 27017), self.client.address)
 
+    ##TODO 
     @unittest.expectedFailure
     def test_duplication(self, ):
-        self.assertEqual(1, 1, "broken")
+        result = self.emb_collection.create_index('Staff', unique=True)
+        self.assertEqual(result, 'Staff')
+
+    ##TODO
+    @unittest.expectedFailure
+    def test_expiration(self, ):
+        self.assertRaises(pymongo.errors.OperationFailure, 
+            self.hygiene_collection.create_index('Timestamp', expireAfterSeconds=30))
 
 if __name__ == '__main__':
     unittest.main()
