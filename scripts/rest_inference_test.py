29510988cd531423fbc224a07ab64a7368c46fce
rupertardo <klauszeng@gmail.com>
2019-09-05 19:02:23 -0400

    update rest_inference_test route

--- scripts/rest_inference_test.py
+++ scripts/rest_inference_test.py
@@ -7,9 +7,9 @@ import time
 import json 
 
 
-url = 'http://172.29.135.101:5000/sanushost/api/v1.0/entry_img'
-# url = 'http://172.29.135.101:5000/sanushost/api/v1.0/sanitizer_img'
-# url = 'http://172.29.135.101:5000/sanushost/api/v1.0/entry_staff_check'
+# url = 'http://192.168.1.101:5000/sanushost/api/v1.0/identify_face'
+# url = 'http://192.168.1.101:5000/sanushost/api/v1.0/update_staff_status_clean'
+url = 'http://192.168.1.101:5000/sanushost/api/v1.0/check_staff_hygiene_status'
 # image = dlib.load_rgb_image('/home/billyzheng/Downloads/klaus2.jpg')
 
 a = time.time()
@@ -21,8 +21,8 @@ print(shape_string)
 image = image.astype(np.float64)
 # image_temp = np.reshape(image, (1, image.shape[0]*image.shape[1]*image.shape[2]))
 img_64 = base64.b64encode(image).decode('ascii')
-payload = {"NodeID": "demo_sanitizer", "Timestamp": time.time(), "Image": img_64, "Shape": shape_string}
-# payload = {"Timestamp": time.time(), "Staff": 'klaus'}
+# payload = {"NodeID": "demo_sanitizer", "Timestamp": time.time(), "Image": img_64, "Shape": shape_string}
+payload = {"Timestamp": time.time(), "StaffList": ['klaus', 'luka', 'steven']}
 
 headers = {'Content-Type': 'application/json', 'Accept': 'text/plain'}
 result = requests.post(url, json=payload, headers=headers)
