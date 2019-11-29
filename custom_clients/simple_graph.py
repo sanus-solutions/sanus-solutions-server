0ad780011c6d4dc06a508ae528c2a0137f2ac712
rupertardo <klauszeng@gmail.com>
2019-11-29 15:10:52 -0500

    update simple graph to fit daycare use case

--- custom_clients/simple_graph.py
+++ custom_clients/simple_graph.py
@@ -2,7 +2,7 @@ import os, sys, time, collections, boto3, datetime
 sys.path.append(os.path.abspath('..'))
 import numpy as np
 from sanus_solutions_server.custom_clients import id_client
-from sanus_solutions_server.custom_clients import mongo_hygiene_client
+# from sanus_solutions_server.custom_clients import druid_client
 """
 minimal implementation of graph structure
 """
@@ -18,70 +18,21 @@ class SimpleGraph():
         self.dist_thresh = 0.15 # TODO: what's a good similarity threshold here????
         # self.id_client = id_client.IdClient()
         self.id_client = id_client
-
-        ## A temporary 
-        self.hygiene_record = mongo_hygiene_client.MongoClient()
+        # self.druid_client = druid_client()
 
     # DEMO USES ONLY METHODS BELOW
-    def check_breach(self, embeddings, timestamp):
-        check_breach_time_start = time.time()
+    def log_staff(self, node_id, embeddings, timestamp):
         staff_list = []
-        for idx, emb in enumerate(embeddings):
-            ## check_staff returns 
-            ## (name, 1) if staff exits
-            ## (none, 0) 
-            staff = self.id_client.check_staff(emb)
-            if staff[1]:
-                current_staff_records = self.hygiene_record.find('Staff', staff[0])
-                if current_staff_records.count(): 
-                    for record in current_staff_records:
-                        current_timestamp = datetime.datetime.fromtimestamp(timestamp).replace(tzinfo=datetime.timezone.utc)
-                        time_diff = abs((record['Timestamp'].replace(tzinfo=datetime.timezone.utc) - current_timestamp).total_seconds())
-                        if time_diff < self.id_client.TIME_THRESH: 
-                            staff_list.append((staff[0], 1)) # (Name, Clean)
-                            break
-                else:
-                    staff_list.append((staff[0], 0)) # (Name, Not clean)
-            else:
-                ## None staff
-                staff_list.append((None, 0)) # (No name, Not clean)
+        for embedding in embeddings:
+            result = self.id_client.check_staff(embedding)
+            if result[1]:
+                staff_id = result[0]
+                # result = self.druid_client.inject(node_id, staff_id, timestamp) 
+                # print(result.json())
+                staff_list.append(staff_id)
         return staff_list
 
-    def check_breach_by_name(self, staff_list, timestamp):
-        result = []
-        for staff in staff_list:
-            current_staff_result = (staff, 0) # (Name, Not clean), default not clean
-            current_staff_records = self.hygiene_record.find('Staff', staff)
-            if current_staff_records.count(): 
-                for record in current_staff_records:
-                    current_timestamp = datetime.datetime.fromtimestamp(timestamp).replace(tzinfo=datetime.timezone.utc)
-                    time_diff = abs((record['Timestamp'].replace(tzinfo=datetime.timezone.utc) - current_timestamp).total_seconds())
-                    if time_diff < self.id_client.TIME_THRESH: 
-                        current_staff_result = (staff, 1) # Replace by 'clean'
-
-            result.append(current_staff_result)
-        return result
-
-    def update_node(self, embeddings, timestamp, node_id):
-        for emb in embeddings:
-            staff = self.id_client.check_staff(emb)
-            if staff[1]:
-                ## Remove this try statement later. Node_ID shall exit when a new dispenser unit is added to the location based graph
-                ## Convert to utc
-                timestamp = datetime.datetime.fromtimestamp(timestamp).replace(tzinfo=datetime.timezone.utc)
-                current_dictionary = {
-                'NodeID' : node_id,
-                'Timestamp' : timestamp,
-                'Staff' : staff[0],
-                }
-                try:
-                    return self.hygiene_record.insert_record(current_dictionary).acknowledged
-                except:
-                    ## TODO
-                    return False
-        return False
-
-    def check_staff(self, embeddings):
+    def check_staff(self, embedding):
         for idx, emb in enumerate(embeddings):
             staff = self.id_client.check_staff(emb)
             if staff[0]:
